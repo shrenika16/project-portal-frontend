6407c7061f9f98035ac7526986e3f8cc46c9a6c7
Shrenika Pampatwar <shrenika0311l@gmail.com>
2026-04-26 23:30:10 +0530

    Fix routing with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -9,42 +9,36 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
 
-        {/* Default */}
         <Route path="/" element={<Navigate to="/login" />} />
 
-        {/* Auth */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
-        {/* Admin */}
         <Route path="/admin" element={
           <ProtectedRoute role="admin">
             <AdminDashboard />
           </ProtectedRoute>
         } />
 
-        {/* Client */}
         <Route path="/client" element={
           <ProtectedRoute role="client">
             <ClientDashboard />
           </ProtectedRoute>
         } />
 
-        {/* User */}
         <Route path="/user" element={
           <ProtectedRoute role="user">
             <UserDashboard />
           </ProtectedRoute>
         } />
 
-        {/* Catch all */}
         <Route path="*" element={<Navigate to="/login" />} />
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
